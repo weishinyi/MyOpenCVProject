7d734b75315dcc7c1c59132c8357589366c5ce52
shin-yi wei <exo123456789@yahoo.com.tw>
2016-09-04 17:56:57 +0800

    face detection test success!

--- app/src/main/java/com/example/gmbug/mycvtest1/MainActivity.java
+++ app/src/main/java/com/example/gmbug/mycvtest1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gmbug.mycvtest1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,21 +12,51 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final Scalar RECT_COLOR = new Scalar(0, 255, 0, 255);
 
-    private String TAG ="OpenCV Test!";
+    private String TAG = "OpenCV";
 
     private CameraBridgeViewBase mOpenCvCameraView;
+    private CascadeClassifier cascadeClassifier;
 
     private Mat rgbaImg;
     private Mat grayImg;
+    private int absoluteFaceSize = 0;
 
+    //BaseLoaderCallback
+    private BaseLoaderCallback myLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status)
+        {
+            switch (status)
+            {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG,"OpenCV loaded successfully!");
+
+                    initializeCascadeClassifier();
+                    mOpenCvCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
 
     @Override
@@ -45,34 +76,15 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     }
 
-    private BaseLoaderCallback myLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status)
-        {
-            switch (status)
-            {
-                case LoaderCallbackInterface.SUCCESS:
-                    Log.i(TAG,"OpenCV loaded successfully!");
-                    mOpenCvCameraView.enableView();
-                    break;
-                default:
-                    super.onManagerConnected(status);
-                    break;
-            }
-        }
-    };
-
     @Override
-    protected void onResume()
-    {
-        Log.i(TAG,"onResume!");
+    protected void onResume() {
+        Log.i(TAG, "onResume!");
         super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0,this,myLoaderCallback);
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, myLoaderCallback);
     }
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         Log.i(TAG, "onPause!");
         super.onPause();
         if(mOpenCvCameraView != null)
@@ -90,14 +102,16 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-        Log.i(TAG,"onCameraViewStarted!");
+        Log.i(TAG, "onCameraViewStarted!");
+
+        //set the size of detection face
+        setAbsoluteFaceSize(height);
     }
 
     @Override
     public void onCameraViewStopped() {
-        Log.i(TAG,"onCameraViewStopped!");
+        Log.i(TAG, "onCameraViewStopped!");
     }
-
 
     /**onCameraFrame function
      * note1: you can process the input frame in this function!
@@ -111,9 +125,74 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         rgbaImg = inputFrame.rgba();
         grayImg = inputFrame.gray();
 
-        //draw a rectangle on frame
-        Imgproc.rectangle(rgbaImg, new Point(0, 0), new Point(100,100),RECT_COLOR,3);
+        MatOfRect faces = new MatOfRect();
+
+        //using classifier to detect face
+        try{
+            if(cascadeClassifier != null)
+            {
+                cascadeClassifier.detectMultiScale(grayImg, faces,1.1,2,2, new Size(absoluteFaceSize,absoluteFaceSize),new Size());
+            }
+        }catch(Exception e){
+            Log.i(TAG,"using classifier to detect face ERROR!");
+        }
+
+
+        //If there are any faces found, draw a rectangle around it
+        try{
+            Rect[] facesArray = faces.toArray();
+
+            for (Rect oneface : facesArray) {
+                //draw a rectangle on frame
+                Imgproc.rectangle(rgbaImg, oneface.tl(), oneface.br(),RECT_COLOR,3);
+            }
+
+        }catch (Exception e){
+            Log.i(TAG,"draw the rectangle on frame Error!");
+        }
 
         return rgbaImg;
     }
-}
+
+    /** initialize Cascade Classifier */
+    private void initializeCascadeClassifier()
+    {
+        try{
+            // Copy the resource into a temp file so OpenCV can load it
+            InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while((bytesRead=is.read(buffer)) != -1)
+            {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+            //Load the CascadeClassifier
+            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            if(cascadeClassifier.empty()){
+                Log.i(TAG,"Failed to load cascade classifier");
+                cascadeClassifier = null;
+            }else{
+                Log.i(TAG,"Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+            }
+
+        }catch (Exception e) {
+            Log.e(TAG,e.getMessage());
+        }
+
+    }
+
+    /** set the size of detection face */
+    private void setAbsoluteFaceSize(int height)
+    {
+        // The faces will be a 20% of the height of the screen
+        absoluteFaceSize = (int)(height *0.2);
+    }
+
+} //end class
